d0df5b54cc406bced61dfa022783f288fa9ba923
Brady Dornan <bradydornan@gmail.com>
2021-06-26 12:29:55 +0900

    split components up into different files for cleaner code, styled dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 // a component in react, is just a javascript function
 // you need to tell react that the css file exists for this component
-import './ExpenseItem.css';
+
 
 //because the date is an object, we need
   //to convert it to a string or itll break everything
@@ -13,21 +13,18 @@ import './ExpenseItem.css';
 
 //to use 'props', you must have it defined in your attributes.
 // for example in App.js, it is defined as title={expenses[0].title}
+import ExpenseDate from './ExpenseDate';
+// we used "./" when we're looking for a file in the same folder.
+import './ExpenseItem.css';
+
 
 function ExpenseItem(props) {
   //if we have complicated long code it's better practice to put it in a variable at the top
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit'});
-  const year = props.date.getFullYear();
-  //getFullYear(); is another built in method which extracts date as a 4-digit number
+  // if there is no content between the component tag, you can just do <ExpenseDate />
 
   return (
     <div className="expense-item">
-     <div>
-       <div>{month}</div>
-       <div>{day}</div>
-       <div>{year}</div>
-     </div>
+    <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
